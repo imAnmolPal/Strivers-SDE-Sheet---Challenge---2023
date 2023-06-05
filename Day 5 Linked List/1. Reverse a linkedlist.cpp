87bd14df8971e5dd87d40a02c25f718f933143bb
imAnmolPal <41437854+imAnmolPal@users.noreply.github.com>
2023-06-05 12:52:52 +0530

    Update 1. Reverse a linkedlist.cpp

--- Day 5 Linked List/1. Reverse a linkedlist.cpp
+++ Day 5 Linked List/1. Reverse a linkedlist.cpp
@@ -1,1 +1,24 @@
-1. Reverse a linkedlist
+// 1. Reverse a linkedlist
+
+// basically we have to reverse the arrows 
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    // Write your code here
+    LinkedListNode *dummy = NULL;
+    while(head!=NULL){
+    	// taking hold of rest of the list
+    	LinkedListNode *next = head->next;
+
+    	// making current node point to prev node
+    	head->next = dummy;
+
+    	// making current node as dummy.
+    	dummy = head;
+
+    	// making next node has head, so that we repeat the process
+    	head = next;
+    }
+
+    return dummy;
+}
